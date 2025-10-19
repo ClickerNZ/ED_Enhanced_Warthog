48fd6efd60f1e329d634a6af5a490042c20d22f9
ClickerNZ <click@monds.co.nz>
2025-10-19 21:56:49 +1300

    Minor updates

--- SyncScriptWH.cmd
+++ SyncScriptWH.cmd
@@ -3,7 +3,7 @@
 cls
 
 echo.
-echo Sync Script files with development folder?
+echo Sync Script files with development folder
 echo.
 
 pause
@@ -11,11 +11,10 @@ pause
 del C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\Output\TTSQueue\*.*
 del C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\Output\TTSQueue\Archive\*.*
 
-rem robocopy C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ScriptFiles /xf C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ED_Usersettings.tmh /L 
-rem robocopy C:\thrustmaster\ed_targetscript_warthog\SupportFiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\SupportFiles\ /s /xd C:\thrustmaster\ed_targetscript_warthog\supportfiles\edmc\edmc_plugins\edmc-ClickersFolly\__pycache__ /L  
+robocopy C:\thrustmaster\ed_targetscript_warthog\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ /s /xf C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ED_Usersettings.tmh /xd C:\thrustmaster\ed_targetscript_warthog\supportfiles\edmc /L 
 
-robocopy C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ScriptFiles /L 
-robocopy C:\thrustmaster\ed_targetscript_warthog\SupportFiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\SupportFiles\ /s /L  
+rem robocopy C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ScriptFiles /L 
+rem robocopy C:\thrustmaster\ed_targetscript_warthog\SupportFiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\SupportFiles\ /s /L  
 
 echo.
 echo if output above is expected, press any key, otherwise press ctrl+c to quit
@@ -23,11 +22,7 @@ echo.
 
 pause 
 
-rem robocopy C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ScriptFiles /xf C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ED_Usersettings.tmh
-rem robocopy C:\thrustmaster\ed_targetscript_warthog\SupportFiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\SupportFiles\ /s /xd C:\thrustmaster\ed_targetscript_warthog\supportfiles\edmc\edmc_plugins\edmc-ClickersFolly\__pycache__
-
-robocopy C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ScriptFiles
-robocopy C:\thrustmaster\ed_targetscript_warthog\SupportFiles\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\SupportFiles\ /s
+robocopy C:\thrustmaster\ed_targetscript_warthog\ D:\Users\Den\OneDrive\Personal\Thrustmaster\TARGET\Clicker\Development\ED_Enhanced_Warthog\ /s /xf C:\thrustmaster\ed_targetscript_warthog\scriptfiles\ED_Usersettings.tmh /xd C:\thrustmaster\ed_targetscript_warthog\supportfiles\edmc
 
 pause
 
