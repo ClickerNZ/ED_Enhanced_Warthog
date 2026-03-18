f8aa60f857e2388fda930ee0425c8d6b35b09505
Clicker <click@monds.co.nz>
2026-03-18 18:39:49 +1300

    Update ProcessJournal.cmd

--- SupportFiles/PowerShell/ProcessJournal.cmd
+++ SupportFiles/PowerShell/ProcessJournal.cmd
@@ -2,7 +2,6 @@
 cls
 echo.
 
-rem start powershell -NoProfile -ExecutionPolicy Bypass -NonInteractive -File "C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\PowerShell\ProcessJournal.ps1"
 powershell -NoProfile -ExecutionPolicy Bypass -NonInteractive -File "C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\PowerShell\ProcessJournal.ps1"
 
 rem pause to catch startup errors
