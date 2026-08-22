a52aef8ce22db12ffed44d38e50376e0a8ec5b07
Clicker <click@monds.co.nz>
2026-08-22 12:14:28 +1200

    Update TTSMonitor.cmd

--- SupportFiles/PowerShell/TTSMonitor.cmd
+++ SupportFiles/PowerShell/TTSMonitor.cmd
@@ -2,11 +2,10 @@
 cls
 echo.
 
-rem start powershell.exe -ExecutionPolicy Bypass -File "C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\PowerShell\TTSMonitor-v21.ps1"
-powershell.exe -ExecutionPolicy Bypass -File "C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\PowerShell\TTSMonitor-v21.ps1"
+powershell.exe -ExecutionPolicy Bypass -File "C:\Thrustmaster\ED_TargetScript_Warthog\SupportFiles\PowerShell\TTSMonitor.ps1"
 
 rem pause to catch startup errors
-pause
+rem pause
 
 exit 
 
